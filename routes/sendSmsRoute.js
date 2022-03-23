5c7db9bc1bc2e0614105937d4b0dbb86ab0f3cfd
Owolabi Toba <toba@elta.solutions>
2022-03-23 11:15:45 +0000

    worked on send message with dojal

--- routes/sendSmsRoute.js
+++ routes/sendSmsRoute.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const { split, join } = require('lodash');
 const axios = require('axios');
 const Member = require("../models/memberModel");
+const FirstTimer = require("../models/firstTimerModel");
 const Department = require("../models/departmentModel");
+const Churchgroup = require("../models/churchGroupModel");
 const { verifyTokenAndAuthorization, verifyTokenAndAdmin, verifyToken } = require("../verifyToken");
 
 
@@ -15,6 +17,7 @@ router.post("/custom", verifyToken, async (req, res) => {
   if (!mobile) return res.status(401).json({ msg: "Mobile Field is Empty" })
   if (!message) return res.status(401).json({ msg: "Message Field is Empty" })
 
+
   // const nos_list = str_replace(array("\n", ":", ";"), ",", mobile);
   const nos_list = mobile;
   const nos = split(nos_list);
@@ -22,6 +25,7 @@ router.post("/custom", verifyToken, async (req, res) => {
 
   const d_list = n_list.join(",");
 
+  console.log("number", d_list)
 
   const url = `${process.env.DOJAH_BASE_URL}/messaging/sms/`;
   const postVars = {
@@ -56,51 +60,109 @@ router.post("/custom", verifyToken, async (req, res) => {
     })
   } catch (error) {
 
-    console.log(error); // this is the main part. Use the response property from the error object
+    // console.log(error); // this is the main part. Use the response property from the error object
 
-    return res.status(500).json({ msg: error });
+    return res.status(500).json({ msg: error.response.data['error'] });
   }
 })
 
 //Send to All Members
-
 router.post("/", verifyToken, async (req, res) => {
-  const { sender_id, mobile, message, department, church_grp
+  const { sender_id, mobile, message, select_all_members, select_all_firsttimers, departments, church_grp
   } = req.body;
   if (!sender_id) return res.status(401).json({ msg: "Sender ID Field is Empty" });
-  // if (!mobile) return res.status(401).json({ msg: "Mobile Field is Empty" });
   if (!message) return res.status(401).json({ msg: "Message Field is Empty" });
-  // if (!department) return res.status(401).json({ msg: "Deparment Field is Empty" });
-  // if (!church_grp) return res.status(401).json({ msg: "Church Group Field is Empty" });
-
 
   const user = req.user;
-  const nos_list = '';
-  if (req.body.all_members) {
-    const all_mems = await Member.find(user._id)
-    console.log("ALLL MEMBER", all_mems)
-
+  var nos_list = '';
+  var idd = '';
+  if (select_all_members) {
+    var all_mems = await Member.find(user._id);
+    // console.log("ALLL MEMBER", all_mems);
     if (all_mems) {
       all_mems.forEach(val => {
         if (val.mobile) {
-          const nos_list = val.mobile + ',';
-          console.log("NUMBER LLLIST", nos_list)
-        } else {
-          return res.status(401).json({ msg: "Member No Not Available" })
+          nos_list += val.mobile + ',';
+          // console.log("NUMBER LLLIST", nos_list)
         }
       })
     } else {
-      return res.status(401).json({ msg: "No Member Find" })
+      return res.status(401).json({ msg: "Member Not Found" })
     }
-  } else {
-    return res.status(401).json({ msg: "No All Member Selected" })
   }
-  const nos = split(nos_list);
+
+  if (select_all_firsttimers) {
+    var all_first_timers = await FirstTimer.find(user._id);
+    // console.log("ALLL FirstTimer", all_mems);
+    if (all_first_timers) {
+      all_first_timers.forEach(val => {
+        if (val.mobile) {
+          nos_list += val.mobile + ',';
+          // console.log("/v LLLIST", nos_list)
+        }
+      })
+    } else {
+      return res.status(401).json({ msg: "FirstTimer Not Found" })
+    }
+  }
+
+  if (departments) {
+    var all_departs = await Department.find({ department: departments });
+    var members = await Member.find(user._id)
+
+    if (all_departs) {
+      all_departs.forEach(async (val) => {
+        idd += val._id + ",";
+        var dept_id = idd.slice(0, -1);
+        if (members) {
+          members.forEach((val) => {
+            if (val.mdepartment) {
+              var Num = val.mdepartment;
+
+              if (Num.includes(dept_id)) {
+                nos_list += val.mobile + ',';
+              }
+            }
+          })
+        }
+      })
+    }
+  }
+
+  if (church_grp) {
+    var all_church_groups = await Churchgroup.find({ group: church_grp });
+    // console.log("groups", all_church_groups)
+    var members = await Member.find(user._id)
+
+    if (all_church_groups) {
+      all_church_groups.forEach(async (val) => {
+        idd += val._id + ",";
+        var grp_id = idd.slice(0, -1);
+
+        if (members) {
+          members.forEach((val) => {
+            if (val.mchurch_group) {
+              var Num = val.mchurch_group;
+              if (Num.includes(grp_id)) {
+                nos_list += val.mobile + ',';
+              }
+            }
+          })
+        }
+      })
+    }
+  }
+
+
+
+  const nos = split(nos_list.slice(0, -1));
   console.log("NUMBEEEERR", nos);
+  // console.log("NUMBEEEERR QWE4 JFJR", nos_list.slice(0,-1));
   const n_list = [...new Set(nos)]
+  // console.log("NUMBEEEERR", n_list);
 
   const d_list = n_list.join(",");
-
+  // console.log("DEEE LIST", d_list);
 
   const url = `${process.env.DOJAH_BASE_URL}/messaging/sms/`;
   const postVars = {
@@ -109,7 +171,6 @@ router.post("/", verifyToken, async (req, res) => {
     'channel': 'sms',
     'sender_id': sender_id
   };
-
   const options = {
     'method': 'POST',
     data: JSON.stringify(postVars),
@@ -120,12 +181,9 @@ router.post("/", verifyToken, async (req, res) => {
     }
   };
 
-
   try {
     let resp = await axios(url, options);
     let data = resp.data;
-    console.log(data);
-    // console.log(resp);
     return res.status(200).json({
       status: {
         code: 100,
@@ -134,10 +192,8 @@ router.post("/", verifyToken, async (req, res) => {
       data: data,
     })
   } catch (error) {
-
-    console.log(error); // this is the main part. Use the response property from the error object
-
-    return res.status(500).json({ msg: error });
+    // console.log(error.response.data['error']); // this is the main part. Use the response property from the error object
+    return res.status(500).json({ msg: error.response.data['error'] });
   }
 
 })
@@ -165,13 +221,11 @@ router.get("/walletbalance", verifyToken, async (req, res) => {
         code: 100,
         msg: 'Wallet Balance Fetched Successfully'
       },
-      wallet_balance: data,
-
+      wallet_balance: data
     })
   } catch (error) {
-    console.log(error); // this is the main part. Use the response property from the error object
-
-    return res.status(500).json({ msg: error });
+    // console.log(error); // this is the main part. Use the response property from the error object
+    return res.status(500).json({ msg: error.response.data['error'] });
   }
 })
 
